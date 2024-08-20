b61b3c815c9bfa116fc98411bcbe6a44e13e8467
quiet-web <qureshiammar900@gmail.com>
2024-08-19 22:17:49 -0700

    Ooops fundamental concept

--- python.py
+++ python.py
@@ -1,1 +1,13 @@
-print("hello world")
+class Employee :
+   
+    def __init__(self,name ,salary,extra_hours=50) :
+        self.name =  name
+        self.salary = salary
+        self.extra_hours = extra_hours
+    def display(self):
+        return display(self.salary)
+
+
+name:str = str(input("Enter name : "))  
+salary:int = int(input("Enter the salary : "))
+  
